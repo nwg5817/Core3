e264c58033f6bf83246396d1e7239e815cf47df6
nwg5817 <nwg5817@gmail.com>
2019-02-25 19:57:37 -0500

    [Update] Village Remove 24 FS points
    Enable removal of FS skills @ Rank 2 Padawan.
    
    Change-Id: I1c85a7138ffaf40c111c46c30652ac4852dc197d

--- MMOCoreORB/bin/scripts/managers/jedi/village_jedi_manager.lua
+++ MMOCoreORB/bin/scripts/managers/jedi/village_jedi_manager.lua
@@ -127,7 +127,7 @@ function VillageJediManager:canSurrenderSkill(pPlayer, skillName)
 		return false
 	end
 
-	if string.find(skillName, "force_sensitive_") and CreatureObject(pPlayer):hasSkill("force_title_jedi_rank_02") and CreatureObject(pPlayer):getForceSensitiveSkillCount(false) <= 24 then
+	if string.find(skillName, "force_sensitive_") and CreatureObject(pPlayer):hasSkill("force_title_jedi_rank_02") and CreatureObject(pPlayer):getForceSensitiveSkillCount(false) <= 0 then
 		return false
 	end
 
